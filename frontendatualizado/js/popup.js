1c8c72b5ecf2d76af23f57684b312a95c75b83bb
banana. <giovanna.rocha.cardoso@gmail.com>
2024-11-19 22:56:40 -0300

    🐶

--- frontendatualizado/js/popup.js
+++ frontendatualizado/js/popup.js
@@ -1,23 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const eventModal = new bootstrap.Modal(document.getElementById('eventModal'));
     const openModalBtn = document.getElementById('openModalBtn');
     const modalFooterBtn = document.getElementById('modalFooterBtn');
-    
+
+
+    window.onload = function () {
+        eventModal.show();
+    };
+
     const lastModalTime = localStorage.getItem('lastModalTime');
     const currentTime = new Date().getTime();
     const showModalAndButton = () => {
-        eventModal.show(); 
+        eventModal.show();
     };
     if (!lastModalTime || (currentTime - lastModalTime > 3 * 60 * 60 * 1000)) {
         showModalAndButton();
         localStorage.setItem('lastModalTime', currentTime);
     }
     eventModal._element.addEventListener('hidden.bs.modal', function () {
-        modalFooterBtn.style.display = 'block';  
+        modalFooterBtn.style.display = 'block';
     });
-    openModalBtn.addEventListener('click', function() {
+    openModalBtn.addEventListener('click', function () {
         eventModal.show();
-        modalFooterBtn.style.display = 'none';  
+        modalFooterBtn.style.display = 'none';
     });
 });
 // Adicionando a funcionalidade de alternância (ligar/desligar)
@@ -25,10 +30,10 @@ const button = document.getElementById('openModalBtn');
 button.addEventListener('click', () => {
     button.classList.toggle('active'); // Alterna a classe 'active' no botão
 });
-document.getElementById('openModalBtn').addEventListener('click', function() {
+document.getElementById('openModalBtn').addEventListener('click', function () {
     // Ativa a classe de explosão
     this.classList.add('exploding');
-    
+
     // Depois que a animação termina, removemos a classe para permitir outro clique
     setTimeout(() => {
         this.classList.remove('exploding');
