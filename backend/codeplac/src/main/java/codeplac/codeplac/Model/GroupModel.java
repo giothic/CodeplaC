7a964478f0815dd2a03ba48cdef075c6f54368c7
thygas07 <thyago.carvalho@esoftware.uniceplac.edu.br>
2024-11-20 14:39:54 -0300

    Adicionado a anotação para preencher a dataInscricao na hora da criação

--- backend/codeplac/src/main/java/codeplac/codeplac/Model/GroupModel.java
+++ backend/codeplac/src/main/java/codeplac/codeplac/Model/GroupModel.java
@@ -1,13 +1,13 @@
 package codeplac.codeplac.Model;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import codeplac.codeplac.Utils.JsonListConverter;
 import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,11 +32,11 @@ public class GroupModel {
   @Column(name = "id_Equipe")
   private int idEquipe;
 
+  @CreationTimestamp
   @Column(name = "data_inscricao")
-  private Instant dataInscricao;
+  private LocalDateTime dataInscricao;
 
-  @Convert(converter = JsonListConverter.class)
-  private List<Member> membros;
+  private String membros;
 
   @Column(name = "nome_equipe")
   private String nomeEquipe;
