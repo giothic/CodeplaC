8a24bab3e857e2d571288de572bc2a09abab564c
Ágatha <139082226+akyparfait@users.noreply.github.com>
2024-11-25 19:29:08 -0300

    arrumando pa

--- frontendatualizado/js/popup.js
+++ frontendatualizado/js/popup.js
@@ -4,25 +4,30 @@ document.addEventListener('DOMContentLoaded', function() {
     const modalFooterBtn = document.getElementById('modalFooterBtn');
     
     const lastModalTime = localStorage.getItem('lastModalTime');
-    const currentTime = new Date().getTime(); 
+    const currentTime = new Date().getTime();
     
+
     const showModalAndButton = () => {
-        eventModal.show(); 
+        eventModal.show();  
     };
 
     if (!lastModalTime || (currentTime - lastModalTime > 3 * 60 * 60 * 1000)) {
+  
         showModalAndButton();
         localStorage.setItem('lastModalTime', currentTime); 
     }
 
+
     modalFooterBtn.style.display = 'none';  
 
+   
     eventModal._element.addEventListener('hidden.bs.modal', function () {
+
         modalFooterBtn.style.display = 'block';  
     });
 
     openModalBtn.addEventListener('click', function() {
         eventModal.show();
-        modalFooterBtn.style.display = 'none';  
+        modalFooterBtn.style.display = 'none'; 
     });
 });
