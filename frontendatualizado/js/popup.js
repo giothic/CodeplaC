a1706b810e5b27ea930fd3309f9347ad5bf525e3
Ágatha <139082226+akyparfait@users.noreply.github.com>
2024-11-25 16:54:04 -0300

    Corrigindo erros do pop-up

--- frontendatualizado/js/popup.js
+++ frontendatualizado/js/popup.js
@@ -13,6 +13,29 @@ document.addEventListener('DOMContentLoaded', function () {
     const showModalAndButton = () => {
         eventModal.show();
     };
+    if (!lastModalTime || (currentTime - lastModalTime > 3 * 60 * 60 * 1000)) {
+        showModalAndButton();
+        localStorage.setItem('lastModalTime', currentTime);
+    }
+    eventModal._element.addEventListener('hidden.bs.modal', function () {
+        modalFooterBtn.style.display = 'block';
+    });
+    openModalBtn.addEventListener('click', function () {
+        eventModal.show();
+        modalFooterBtn.style.display = 'none';
+    });
+});
+// Adicionando a funcionalidade de alternância (ligar/desligar)
+const button = document.getElementById('openModalBtn');
+button.addEventListener('click', () => {
+    button.classList.toggle('active'); // Alterna a classe 'active' no botão
+});
+document.getElementById('openModalBtn').addEventListener('click', function () {
+    // Ativa a classe de explosão
+    this.classList.add('exploding');
 
-
+    // Depois que a animação termina, removemos a classe para permitir outro clique
+    setTimeout(() => {
+        this.classList.remove('exploding');
+    }, 400); // O tempo deve ser igual ao da animação (0.4s)
 });
