d1c4c9c9c365f79b4c75afde6f704ac8b818e21a
Ágatha <139082226+akyparfait@users.noreply.github.com>
2024-11-25 19:22:24 -0300

    Mudando pop-up

--- frontendatualizado/js/popup.js
+++ frontendatualizado/js/popup.js
@@ -6,29 +6,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const lastModalTime = localStorage.getItem('lastModalTime');
     const currentTime = new Date().getTime(); 
     
-
     const showModalAndButton = () => {
-        eventModal.show();  
+        eventModal.show(); 
     };
 
-   
     if (!lastModalTime || (currentTime - lastModalTime > 3 * 60 * 60 * 1000)) {
-
         showModalAndButton();
         localStorage.setItem('lastModalTime', currentTime); 
     }
 
-    openModalBtn.style.display = 'block';
-
     modalFooterBtn.style.display = 'none';  
 
     eventModal._element.addEventListener('hidden.bs.modal', function () {
-
         modalFooterBtn.style.display = 'block';  
     });
 
     openModalBtn.addEventListener('click', function() {
         eventModal.show();
-        modalFooterBtn.style.display = 'none'; 
+        modalFooterBtn.style.display = 'none';  
     });
 });
